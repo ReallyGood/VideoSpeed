4536390df219e9e520ebd27c12f5daa585c27ea3
Ronny Orbach <ronny@reallygood.co.il>
2013-11-03 17:13:56 +0200

    Improvements

--- chrome/src/inject/inject.js
+++ chrome/src/inject/inject.js
@@ -1,8 +1,11 @@
 console.log("Hi from video trainer!");
 
 (function(){
+
+    var speeds = [0.75, 1, 1.25];
     var videoEl = $('video')[0];
     var watchLaterEl = $('.html5-watch-later-button');
+    var speedInput, speedValue;
 
     if($('.html5-watch-later-button').length) {
         inject();
@@ -16,6 +19,8 @@ console.log("Hi from video trainer!");
     }
 
     function inject(){
+        speedInput = $('<input type="range" class="speed" value="1.0" min="0.5" max="1.5" step="0.1">');
+        speedValue = $('<strong>Speed: &times;<span class="current-speed">1.0</span></strong>');
         var speedchangeButton = watchLaterEl
             .clone()
             .attr('data-tooltip', 'Change Video Speed')
@@ -23,13 +28,21 @@ console.log("Hi from video trainer!");
             .removeClass('html5-watch-later-button')
             .addClass('html5-change-speed-button')
             .find('.yt-uix-button-icon-wrapper')
-                .append('<strong>&times;<span class="speed">1.00</span></strong>')
+                .append(speedValue)
+                .append(speedInput)
                 .end()
             .insertAfter(watchLaterEl);
+        speedInput.on('change', onSpeedChange);
     }
 
     function changeSpeed(speed){
         videoEl.playbackRate = speed;
+    }
+
+    function onSpeedChange(ev){
+        var speed = +ev.target.value;
+        changeSpeed(speed);
+        speedValue.find('.current-speed').text( speed.toFixed(1) );
     }
 
 })();
